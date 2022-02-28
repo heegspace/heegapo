f7831720a40adb292d44097e5b9e9bdc637ff198
dataspace <dataspace@126.com>
2022-02-28 17:33:52 +0800

    feature: add float32 and float64

--- reader.go
+++ reader.go
@@ -20,6 +20,10 @@ func (obj aReader) Int(def int) int {
 		return int(obj.conf.(int))
 	}
 
+	if _,ok := obj.conf.(int64); ok {
+		return int(obj.conf.(int64))
+	}
+
 	if _,ok := obj.conf.(string); !ok {
 		return int(def)
 	}
@@ -33,6 +37,10 @@ func (obj aReader) Int(def int) int {
 }
 
 func (obj aReader) Int64(def int64) int64 {
+	if _,ok := obj.conf.(int); ok {
+		return int64(obj.conf.(int))
+	}
+
 	if _,ok := obj.conf.(int64); ok {
 		return int64(obj.conf.(int64))
 	}
@@ -44,6 +52,64 @@ func (obj aReader) Int64(def int64) int64 {
 	value, err := strconv.ParseInt(obj.conf.(string), 10, 64)
 	if nil != err {
 		return int64(def)
+	}
+
+	return value
+}
+
+func (obj aReader) Float32(def float32) float32 {
+	if _,ok := obj.conf.(int); ok {
+		return float32(obj.conf.(int))
+	}
+
+	if _,ok := obj.conf.(int64); ok {
+		return float32(obj.conf.(int64))
+	}
+
+	if _,ok := obj.conf.(float32); ok {
+		return float32(obj.conf.(float32))
+	}
+
+	if _,ok := obj.conf.(float64); ok {
+		return float32(obj.conf.(float64))
+	}
+
+	if _,ok := obj.conf.(string); !ok {
+		return float32(def)
+	}
+
+	value, err := strconv.ParseFloat(obj.conf.(string),32)
+	if nil != err {
+		return float32(def)
+	}
+
+	return float32(value)
+}
+
+func (obj aReader) Float64(def float64) float64 {
+	if _,ok := obj.conf.(int); ok {
+		return float64(obj.conf.(int))
+	}
+
+	if _,ok := obj.conf.(int64); ok {
+		return float64(obj.conf.(int64))
+	}
+	
+	if _,ok := obj.conf.(float32); ok {
+		return float64(obj.conf.(float32))
+	}
+
+	if _,ok := obj.conf.(float64); ok {
+		return float64(obj.conf.(float64))
+	}
+
+	if _,ok := obj.conf.(string); !ok {
+		return float64(def)
+	}
+
+	value, err := strconv.ParseFloat(obj.conf.(string), 64)
+	if nil != err {
+		return float64(def)
 	}
 
 	return value
