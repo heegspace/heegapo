3073b8c36fe83f0853a847356c8084ac3498b92f
dataspace <dataspace@126.com>
2022-02-28 17:21:21 +0800

    fix: 修复读取int or int64 panic问题

--- reader.go
+++ reader.go
@@ -16,6 +16,14 @@ func (obj aReader) String(def string) string {
 }
 
 func (obj aReader) Int(def int) int {
+	if _,ok := obj.conf.(int); ok {
+		return int(obj.conf.(int))
+	}
+
+	if _,ok := obj.conf.(string); !Ok {
+		return int(def)
+	}
+
 	value, err := strconv.ParseInt(obj.conf.(string), 10, 64)
 	if nil != err {
 		return int(def)
@@ -25,6 +33,14 @@ func (obj aReader) Int(def int) int {
 }
 
 func (obj aReader) Int64(def int64) int64 {
+	if _,ok := obj.conf.(int64); ok {
+		return int(obj.conf.(int64))
+	}
+
+	if _,ok := obj.conf.(string); !Ok {
+		return int64(def)
+	}
+
 	value, err := strconv.ParseInt(obj.conf.(string), 10, 64)
 	if nil != err {
 		return int64(def)
